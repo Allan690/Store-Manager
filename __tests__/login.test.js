d05ae36d86be6295ea1d2593a20126ece0f3faf4
Allan069 <allannelly690@gmail.com>
2018-11-28 17:55:24 +0300

    [ch #161865049] Wrote BDD tests for the DOM manipulation during login

--- __tests__/login.test.js
+++ __tests__/login.test.js
@@ -4,13 +4,14 @@ describe('login',() => {
 
     beforeEach(() => {
         document.body.innerHTML +=`
-        <div id="notify"></div>
-        <form id="login">
+        <div id="notify">
+          </div>
+        <form id="signin">
           <input type="email" id="email" value="test@gmail.com">
-          <input type="password"  id="password" value ="testing1234">
+          <input type="password"  id="password" value ="test1234">
           <input type="submit" id="submit">
         </form>`;
-        fetchMock = jest.spyOn(global, 'fetch');
+        fetchMock = jest.spyOn(global,'fetch');
         fetchMock.mockImplementation(() =>Promise.resolve ({
             json: () => Promise.resolve({Message:"User logged in successfully!"})
         }));
@@ -23,49 +24,102 @@ describe('login',() => {
         assignMock.mockRestore();
         jest.resetModules()
     });
-    it('fetch data and change the content of #notify', async () =>{
+    it('fetch data and change the content of #notify', async function() {
         document.getElementById('submit').click();
         expect(fetchMock).toHaveBeenCalledTimes(1);
         const fetchArgs = fetchMock.mock.calls[0];
         expect(fetchArgs[0]).toBe('https://store-manager-api-app-v2.herokuapp.com/api/v2/auth/login');
         expect(fetchArgs[1]).toEqual({
             method: "POST",
-            mode: "cors",
             headers: {
-                "Content-Type": "application/json",
+                'Content-Type': 'application/json',
                 'Access-Control-Allow-Origin':'*',
                 'Access-Control-Request-Method': '*'
             },
+            mode: "cors",
             body: JSON.stringify({
                 email: 'test@gmail.com',
-                password: "testing1234"
+                password: "test1234"
             })
 
         });
-        expect.assertions(1);
-        await Promise.resolve().then();
-        expect(document.getElementById('notify').innerHTML).toBe(
-            `<div class="isa_success">
-            <i class="fa fa-check"></i>
-        User logged in successfully!
-        </div>`);
-        expect(assignMock).toHaveBeenCalledTimes(1);
-        expect(assignMock.mock.calls[0][0]).toBe("./index.html");
+     await Promise.resolve().then();
+     await Promise.resolve().then();
+     expect(document.getElementById('notify').innerHTML).toBe(
+         `<div class="isa_success">
+                               <i class="fa fa-check"></i>
+     User logged in successfully!
+    </div>`);
+     expect(localStorage.getItem("token")).not.toBeNull();
+     expect(assignMock).toHaveBeenCalledTimes(1);
+     expect(assignMock.mock.calls[0][0]).toBe("../HTML/index.html");
     });
-    it('Login with wrong email/password', async () =>{
+
+    it('Login with wrong password but correct email', async () =>{
         fetchMock = jest.spyOn(global,'fetch');
         fetchMock.mockImplementation(() =>Promise.resolve ({
-            json: () => Promise.resolve({Message:"User not found!"})
+            json: () => Promise.resolve({Message:"User not found! Check your login details"})
         }));
         document.getElementById('submit').click();
         expect(fetchMock).toHaveBeenCalledTimes(1);
         const fetchArgs = fetchMock.mock.calls[0];
         expect(fetchArgs[0]).toBe('https://store-manager-api-app-v2.herokuapp.com/api/v2/auth/login');
         await Promise.resolve().then();
-        expect(document.getElementById('notify').innerHTML).toBe( '<div class="isa_info">\n  ' +
-            ' <i class="fa fa-info-circle"></i>\n ' +
-            'User not found!\n' +
-            '</div>');
+        await Promise.resolve().then();
+        expect(document.getElementById('notify').innerHTML).toBe(`<div class="isa_info">
+                        <i class="fa fa-info-circle"></i>
+                        User not found! Check your login details
+                         </div>`);
     });
 
+    it('Login with inexistent email', async () =>{
+        fetchMock = jest.spyOn(global,'fetch');
+        fetchMock.mockImplementation(() =>Promise.resolve ({
+            json: () => Promise.resolve({Message:" User not found!"})
+        }));
+        document.getElementById('submit').click();
+        expect(fetchMock).toHaveBeenCalledTimes(1);
+        const fetchArgs = fetchMock.mock.calls[0];
+        expect(fetchArgs[0]).toBe('https://store-manager-api-app-v2.herokuapp.com/api/v2/auth/login');
+        await Promise.resolve().then();
+        await Promise.resolve().then();
+        expect(document.getElementById('notify').innerHTML).toBe(`<div class="isa_info">
+                        <i class="fa fa-info-circle"></i>
+                         User not found!
+                         </div>`);
+    });
+
+    it('Login with email and password fields empty', async () =>{
+        fetchMock = jest.spyOn(global,'fetch');
+        fetchMock.mockImplementation(() =>Promise.resolve ({
+            json: () => Promise.resolve({Message:"Email is required!"})
+        }));
+        document.getElementById('submit').click();
+        expect(fetchMock).toHaveBeenCalledTimes(1);
+        const fetchArgs = fetchMock.mock.calls[0];
+        expect(fetchArgs[0]).toBe('https://store-manager-api-app-v2.herokuapp.com/api/v2/auth/login');
+        await Promise.resolve().then();
+        await Promise.resolve().then();
+        expect(document.getElementById("notify").innerHTML).toBe(`<div class="isa_info">
+                        <i class="fa fa-info-circle"></i>
+                        Email is required!
+                         </div>`);
+    });
+
+    it('Login with password field empty', async () =>{
+        fetchMock = jest.spyOn(global,'fetch');
+        fetchMock.mockImplementation(() =>Promise.resolve ({
+            json: () => Promise.resolve({Message:"password is required"})
+        }));
+        document.getElementById('submit').click();
+        expect(fetchMock).toHaveBeenCalledTimes(1);
+        const fetchArgs = fetchMock.mock.calls[0];
+        expect(fetchArgs[0]).toBe('https://store-manager-api-app-v2.herokuapp.com/api/v2/auth/login');
+        await Promise.resolve().then();
+        await Promise.resolve().then();
+        expect(document.getElementById("notify").innerHTML).toBe(`<div class="isa_info">
+                        <i class="fa fa-info-circle"></i>
+                        password is required
+                         </div>`);
+    });
 });
